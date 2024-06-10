0efb0659e367a7cdd99bd77da41bba6251f845c2
gregory2023 <gecanoram@gmail.com>
2024-06-10 17:15:38 +0200

    agrego BD de prueba

---
+++ Cosultas_sql.sql
@@ -0,0 +1,16 @@
+--selects mas basicos para manipular la BD Northwind de prueba 
+
+--TABLAS EN LA BD 
+
+
+--Products;
+--categories;
+--customers;
+--Employees;
+--Shippers;
+--Suppliers;
+
+
+-- 
+SELECT *
+FROM Products
